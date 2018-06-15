7f09241113416a03216f8b94dc08302dd5e2390c
Ciprian Focsaneanu <ciprian.focsaneanu@keysight.com>
2018-06-15 09:54:11 +0300

    Update keras serving

--- object-context-recognition/keras-server/run_keras_server.py
+++ object-context-recognition/keras-server/run_keras_server.py
@@ -1,3 +1,6 @@
+'''
+Author: Focsaneanu Andrei-Ciprian
+'''
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import keras.backend as K
@@ -7,75 +10,70 @@ import flask
 import io
 import argparse
 
-# initialize our Flask application and the Keras model
+# Initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
 model = None
 
+# Hardcode WIDER 19 classes
 classes = ['Baseball', 'Car Accident', 'Ceremony', 'Concerts', 'Football', 'Gymnastics', 'Hockey', 'Ice Skating', 'Jockey',
 'Matador Bullfighter', 'Meeting', 'People marching', 'Picnic', 'Row Boat', 'Soccer', 'Soldier Patrol', 'Spa', 'Surgeons', 'Swimming']
 
 def load_pretrained_model(pretrained_model, **args):
 	K.clear_session()
-	# load the pre-trained Keras model
+	# Load the pre-trained Keras model
 	global model
 	print ('Loading model: ' + pretrained_model)
 	model = load_model(pretrained_model)
 	model._make_predict_function()
 
 def prepare_image(image, target):
-	# if the image mode is not RGB, convert it
+	# If the image mode is not RGB, convert it
 	if image.mode != "RGB":
 		image = image.convert("RGB")
 
-	# resize the input image and preprocess it
+	# Resize the input image and preprocess it
 	image = image.resize(target)
-	print (image)
 	image = img_to_array(image)
-	print (image.shape)
 	image = np.expand_dims(image, axis=0)
-	print (image.shape)
 	image = np.divide(image, 255.)
-	# image = imagenet_utils.preprocess_input(image)3
 
-	# return the processed image
 	return image
 
+# Implement POST method
 @app.route("/predict", methods=["POST"])
 def predict():
-	# initialize the data dictionary that will be returned from the
-	# view
+	# Initialize the data dictionary that will be returned from the
 	data = {"success": False}
 
-	# ensure an image was properly uploaded to our endpoint
+	# Ensure an image was properly uploaded to our endpoint
 	if flask.request.method == "POST":
 		if flask.request.files.get("image"):
-			# read the image in PIL format
+			# Read the image in PIL format
 			image = flask.request.files["image"].read()
 			image = Image.open(io.BytesIO(image))
 
-			# preprocess the image and prepare it for classification
+			# Preprocess the image and prepare it for classification
 			image = prepare_image(image, target=(224, 224))
 
-			# classify the input image and then initialize the list
+			# Classify the input image and then initialize the list
 			# of predictions to return to the client
 			preds = model.predict(image)
-			# print (preds)
-			# results = imagenet_utils.decode_predictions(preds)
 			data["predictions"] = []
 
 			for i in range(0, len(preds[0])):
 				r = {"label": classes[i], "probability": float(preds[0][i])}
 				data["predictions"].append(r)
 
-			# take the top-5 predictions
+			# Take the top-5 predictions
 			data["predictions"] = list(reversed(sorted(data["predictions"], key=lambda k: k['probability'])))[:5]
-			# indicate that the request was a success
+
+			# Indicate that the request was a success
 			data["success"] = True
 
-	# return the data dictionary as a JSON response
+	# Return the data dictionary as a JSON response
 	return flask.jsonify(data)
 
-# if this is the main thread of execution first load the model and
+# If this is the main thread of execution first load the model and
 # then start the server
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
--- object-context-recognition/keras-server/simple_request.py
+++ object-context-recognition/keras-server/simple_request.py
@@ -1,28 +1,23 @@
-# USAGE
-# python simple_request.py
-
-# import the necessary packages
+'''
+Author: Focsaneanu Andrei-Ciprian
+'''
 import requests
 
-# initialize the Keras REST API endpoint URL along with the input
-# image path
+# Initialize the Keras REST API endpoint URL along with the input image path
 KERAS_REST_API_URL = "http://localhost:5000/predict"
 IMAGE_PATH = "../newWIDER/test/Concerts/85.jpg"
 
-# load the input image and construct the payload for the request
+# Load the input image and construct the payload for the request
 image = open(IMAGE_PATH, "rb").read()
 payload = {"image": image}
 
-# submit the request
+# Submit the request
 r = requests.post(KERAS_REST_API_URL, files=payload).json()
 
-# ensure the request was sucessful
+# Ensure the request was sucessful
 if r["success"]:
 	# loop over the predictions and display them
 	for (i, result) in enumerate(r["predictions"]):
-		print("{}. {}: {:.4f}".format(i + 1, result["label"],
-			result["probability"]))
-
-# otherwise, the request failed
+		print("{}. {}: {:.4f}".format(i + 1, result["label"], result["probability"]))
 else:
 	print("Request failed")
